d055af8970e246f3ea9b0bf46c1918aabd5e0be2
Your Name <omoitukanai1225l@example.com>
2024-10-26 21:54:08 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    const urls = message.content.match(urlRegex);
+
+    if (urls) {
+        const targetChannel = message.guild.channels.cache.find(channel => channel.name === 'target-channel');
+        if (targetChannel) {
+            targetChannel.send(`URL detected: ${urls.join(', ')}`);
+            message.edit(message.content.replace(urlRegex, ''));
+        }
+    }
+});
+
+client.login('YOUR_BOT_TOKEN');
